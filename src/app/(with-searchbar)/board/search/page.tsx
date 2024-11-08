6c6e0662445db6bfec77ec09ed6cc304415331ac
soieu <102034740+soieu@users.noreply.github.com>
2024-11-08 14:11:30 +0900

    search 페이지 api 연결

--- src/app/(with-searchbar)/board/search/page.tsx
+++ src/app/(with-searchbar)/board/search/page.tsx
@@ -1,8 +1,19 @@
+import BoardPreviewColumn from "@/components/board-preview-column";
+
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{ query: string }>;
 }) {
   const { query } = await searchParams;
-  return <div>{query} 검색</div>;
+  const boardsPreview = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/boards/search?query=${query}`
+  ).then((res) => res.json());
+  return (
+    <div>
+      {boardsPreview.map((boardPreview, i) => (
+        <BoardPreviewColumn key={i} {...boardPreview}></BoardPreviewColumn>
+      ))}
+    </div>
+  );
 }
