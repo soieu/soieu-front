dd7efa0db05f18a40bbd5c39f66faebc49eefdbb
soieu <102034740+soieu@users.noreply.github.com>
2024-11-08 14:13:32 +0900

    board 상세 페이지 api 연결

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,4 +1,13 @@
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = await params;
-  return <div>board info : {id}</div>;
+  const boardData = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/boards/${id}`
+  ).then((res) => res.json());
+  return (
+    <>
+      <div>{boardData.title}</div>
+      <div>{boardData.createdDate}</div>
+      <div>{boardData.content}</div>
+    </>
+  );
 }
